904d41ab91c44e80b30db56ad6f292c9f88e7360
Mike352-design <patrickamau702@gmail.com>
2024-12-31 03:26:12 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,5 @@ module.exports = {
 SESSION_ID: 'HANSAMAL-MD=LM8knZzI#XzmMfKfaNAh2OPa8vZMpIRXz_1HFlhO9koiZQco85F4',
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'YOUR GIT TOKEN' : process.env.GITHUB_AUTH_TOKEN,
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'YOUR GIT USER NAME' : process.env.GITHUB_USER_NAME,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? 'YOUR NUMBER' : process.env.OWNER_NUMBER
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '254104916091' : process.env.OWNER_NUMBER
 };
